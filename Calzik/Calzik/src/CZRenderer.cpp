652307242b06ab11b822e3e726462faf1fb1b1df
Ethan Baedke <ethanbaedke@gmail.com>
2025-02-25 11:30:12 -0700

    Draw triangle

--- Calzik/Calzik/src/CZRenderer.cpp
+++ Calzik/Calzik/src/CZRenderer.cpp
@@ -33,6 +33,38 @@ CZRenderer::CZRenderer(HWND hwnd)
     viewport.MinDepth = 0.0f;
     viewport.MaxDepth = 1.0f;
     mDeviceContext->RSSetViewports(1, &viewport);
+
+    ID3DBlob* vsBlob = nullptr;
+    ID3DBlob* psBlob = nullptr;
+    CompileShader(mVertexShaderSource, "main", "vs_5_0", &vsBlob);
+    CompileShader(mPixelShaderSource, "main", "ps_5_0", &psBlob);
+
+    mDevice->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &mVertexShader);
+    mDevice->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &mPixelShader);
+    mDeviceContext->VSSetShader(mVertexShader, 0, 0);
+    mDeviceContext->PSSetShader(mPixelShader, 0, 0);
+
+    // Define input layout
+    D3D11_INPUT_ELEMENT_DESC layout[] = {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+    };
+    mDevice->CreateInputLayout(layout, 2, vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &mInputLayout);
+    mDeviceContext->IASetInputLayout(mInputLayout);
+
+    vsBlob->Release();
+    psBlob->Release();
+
+    // Create vertex buffer
+    Vertex vertices[] = {
+        {  0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f },
+        {  0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f },
+        { -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f }
+    };
+    D3D11_BUFFER_DESC bd = {};
+    bd.ByteWidth = sizeof(vertices);
+    D3D11_SUBRESOURCE_DATA initData = { vertices };
+    mDevice->CreateBuffer(&bd, &initData, &mVertexBuffer);
 }
 
 void CZRenderer::Render()
@@ -41,6 +73,26 @@ void CZRenderer::Render()
     float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
     mDeviceContext->ClearRenderTargetView(mRenderTargetView, clearColor);
 
+    UINT stride = sizeof(Vertex);
+    UINT offset = 0;
+    mDeviceContext->IASetVertexBuffers(0, 1, &mVertexBuffer, &stride, &offset);
+    mDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    mDeviceContext->Draw(3, 0);
+
     // Present the Frame
     mSwapChain->Present(1, 0);
+}
+
+HRESULT CZRenderer::CompileShader(const char* source, const char* entryPoint, const char* target, ID3DBlob** shaderBlob) {
+    ID3DBlob* errorBlob = nullptr;
+    HRESULT hr = D3DCompile(source, strlen(source), nullptr, nullptr, nullptr, entryPoint, target, 0, 0, shaderBlob, &errorBlob);
+    if (FAILED(hr)) {
+        if (errorBlob) {
+            OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+            errorBlob->Release();
+        }
+        return hr;
+    }
+    if (errorBlob) errorBlob->Release();
+    return S_OK;
 }
--- Calzik/Calzik/src/CZRenderer.h
+++ Calzik/Calzik/src/CZRenderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <d3d11.h>
+#include <d3dcompiler.h>
 
 class CZRenderer
 {
@@ -14,4 +15,45 @@ private:
 	ID3D11Device* mDevice;
 	ID3D11DeviceContext* mDeviceContext;
 	ID3D11RenderTargetView* mRenderTargetView;
+	ID3D11Buffer* mVertexBuffer = nullptr;
+	ID3D11VertexShader* mVertexShader = nullptr;
+	ID3D11PixelShader* mPixelShader = nullptr;
+	ID3D11InputLayout* mInputLayout = nullptr;
+
+	struct Vertex
+	{
+		float x, y, z;
+		float r, g, b;
+	};
+
+	// Vertex Shader (HLSL)
+	const char* mVertexShaderSource = R"(
+    struct VS_INPUT {
+        float3 pos : POSITION;
+        float3 color : COLOR;
+    };
+    struct VS_OUTPUT {
+        float4 pos : SV_POSITION;
+        float4 color : COLOR;
+    };
+    VS_OUTPUT main(VS_INPUT input) {
+        VS_OUTPUT output;
+        output.pos = float4(input.pos, 1.0);
+        output.color = float4(input.color, 1.0);
+        return output;
+    }
+    )";
+
+    // Pixel Shader (HLSL)
+    const char* mPixelShaderSource = R"(
+    struct PS_INPUT {
+        float4 pos : SV_POSITION;
+        float4 color : COLOR;
+    };
+    float4 main(PS_INPUT input) : SV_TARGET {
+        return input.color;
+    }
+    )";
+
+    HRESULT CompileShader(const char* source, const char* entryPoint, const char* target, ID3DBlob** shaderBlob);
 };
